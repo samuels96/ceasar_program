a0fb0956c823a91faf34dbc17e2bf29ee9f304be
samuels96 <37853844+samuels96@users.noreply.github.com>
2018-07-29 17:53:21 +0200

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 def main():
-    print("\nDo you wish to encrypt new password or decrypt existing one?\n")
+    print("\nDo you wish to encrypt new entry or decrypt existing one?\n")
     time.sleep(0.1)
     print("1.Encrypt")
     time.sleep(0.1)
@@ -13,11 +13,11 @@ def main():
         try:
             x = int(input())
             if x == 1:
-                sys.stdout.write("Enter password you wish to be encrypted: "),
+                sys.stdout.write("Enter entry you wish to be encrypted: "),
                 res = encrypt(str(input()))
                 print ( "\nEncrypted: {}".format(res))
                 time.sleep(0.05)
-                slow_print("\nEncrypted password saved to keys.txt\n",0.02)
+                slow_print("\nEncrypted entry saved to keys.txt\n",0.02)
                 main()
             if x == 2:
                 decrypt_main()
